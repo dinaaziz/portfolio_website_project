575d5a6d6700e4d9f0de5a3f2a9720fd03b587c0
dina aziz <dina.aziz100@yahoo.com>
2017-04-12 08:08:27 +0000

    add playlist

--- js/interest.js
+++ js/interest.js
@@ -1,14 +1,18 @@
-var channelName = 'TheNewBaghdad';
+var channelName = 'alxs1aa';
 var vidWidth=500;
 var vidHeight=400;
 var vidResults=10;
+
+// https://www.googleapis.com/youtube/v3/channels?part=contentDetails&key=AIzaSyBdoyirpMDZ4o3s0Ib4RP-CYQhElXMEpvM&forUsername=alxs1aa
+
 $(document).ready(function(){
   $.get(
     "https://www.googleapis.com/youtube/v3/channels",{
       part: 'contentDetails',
       forUsername: channelName,
-      key: 'AIzaSyBu7T8r9bITDNnoTUfsyzwV-hMBe4TRwak'},
+      key: 'AIzaSyBdoyirpMDZ4o3s0Ib4RP-CYQhElXMEpvM'},
       function(data){
+        console.dir(data);
        $.each(data.items,function(i,item){
          console.log(item);
          pid= item.contentDetails.relatedPlaylists.uploads;
@@ -25,11 +29,12 @@ $(document).ready(function(){
         playlistId: pid,
         key: 'AIzaSyBu7T8r9bITDNnoTUfsyzwV-hMBe4TRwak'},
         function(data){
+          console.log("second function")
           var output;
          $.each(data.items,function(i,item){
            console.log(item);
           videoTitle=item.snippet.title;
-          output= '<li><iframe height='+vidHeight+'  width='+vidWidth+' src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+          output= '<li><iframe height='+vidHeight+'  width='+vidWidth+' src=\"//www.youtube.com/embed/'+item.snippet.resourceId.videoId'\"></iframe></li>';
           $('#results').append(output);
          })
        })
